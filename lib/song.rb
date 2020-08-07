2338e8e79410429215aad631458ea84a257bc5a6
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-07 02:22:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,27 +27,31 @@ def self.genres
  @@artists.uniq
  end 
  
-  def self.genre_count
-   genres= {}
-   @@genres.each do |genre|
-      if genres.count(genre)
-        genre_count[genre] += 1
-      else 
-        genre_count[genre] = 1
- end 
-end
-genre_count
-end 
- 
  def self.artist_count
-   artists= {}
-   @@artists.each do |artist|
-      if artists.count(artist)
-        artist_count[artist] += 1
-      else 
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
         artist_count[artist] = 1
- end 
-end
-artist_count
-end
+      end
+    end
+    artist_count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
